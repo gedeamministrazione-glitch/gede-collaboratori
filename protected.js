91f9d92fb2082041923b222cd77ef1dca03a5539
gedeamministrazione-glitch <gede.amministrazione@gmail.com>
2025-11-18 19:54:15 +0100

    Update protected.js

--- protected.js
+++ protected.js
@@ -1,4 +1,5 @@
 // Blocco l'accesso se l'utente NON è loggato
 if (!localStorage.getItem("gede_logged_user")) {
-    window.location.href = "/login.html";
+    window.location.href = "/Login.html";
 }
+
